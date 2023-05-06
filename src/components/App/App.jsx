e7018b2559823571bd92c03493617554a34d0707
Galina Guseva <galaguss@yandex.ru>
2023-05-07 00:30:31 +0500

    router for pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import Main from "../Main/Main";
-import Policy from "../Policy/Policy";
+import Header from "../Header/Header";
+import FirstScreen from '../FirstScreen/FirstScreen';
+import Preferences from '../Preferences/Preferences';
+import About from '../About/About';
+import Tocall from '../Tocall/Tocall';
+import Quest from '../Quest/Quest';
+import CallToAction from '../CallToAction/CallToAction';
+import Footer from '../Footer/Footer';
+import PopupWithForm from "../PopupWithForm/PopupWithForm";
 
 const App = () => {
+  const [isPopupWithFormOpen, setIsPopupWithFormOpen] = React.useState(false);
 
-return (   
-      <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="policy" element={<Policy />} />    
-      </Routes>    
+  function handlePopupClick(e) {
+    setIsPopupWithFormOpen(true);
+  } 
+
+  function closeAllPopups() {  
+    setIsPopupWithFormOpen(false);        
+  } 
+  
+  function handleSend({userName, job, email, phone}) {
+    console.log(userName, job, email, phone);
+  };
+  
+
+return (
+  <>
+      <Header onSendClick={handlePopupClick}/>
+      <FirstScreen onUpClick={handlePopupClick}/>         
+      <About />
+      <Preferences />
+      <Tocall/> 
+      <Quest />
+      <CallToAction/>
+      <Footer/> 
+      <PopupWithForm isOpen={isPopupWithFormOpen} onClose={closeAllPopups} onSubmit={handleSend} />        
+</>  
+        
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Policy from "./components/Policy/Policy";
+import NotFound from "./components/NotFound/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+root.render( 
+    <Router basename="/Atlas1/">
+      <Routes>
+      <Route exact path="/" element={<App />} />      
+      <Route exact path="/policy" element={<Policy />} />
+      <Route path="/*" element={<NotFound />} />   
+      </Routes>      
+    </Router>  
 );
 
 // If you want to start measuring performance in your app, pass a function
