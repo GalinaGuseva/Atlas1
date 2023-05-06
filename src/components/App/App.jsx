6fc4a34c4cb0f0cb57bdd39609dbf836c63147fd
Galina Guseva <galaguss@yandex.ru>
2023-05-06 19:00:55 +0500

    path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from "../Main/Main";
 import Policy from "../Policy/Policy";
 
 const App = () => {
 
-return (
-  <BrowserRouter>    
+return (   
       <Routes>
       <Route path="/" element={<Main />} />
       <Route path="policy" element={<Policy />} />    
-      </Routes>  
-    </BrowserRouter>
+      </Routes>    
   );
 }
 
